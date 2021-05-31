1afa6a7edb45330c3febf8db7a4315c4ea51a425
LeonardoBatistaCariasMeli <leonardo.carias@mercadolivre.com>
2021-05-31 16:27:16 -0300

    First commit

---
+++ src/main/java/br/com/digitalhouse/desafiospringapi/DesafioSpringApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.desafiospringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digitalhouse/desafiospringapi/DesafioSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.desafiospringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
