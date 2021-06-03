90248d9f01812e37c9fcb936c383acf885c5f49d
LeonardoBatistaCariasMeli <leonardo.carias@mercadolivre.com>
2021-06-03 13:48:01 -0300

    Ajustes feature  US 0009

--- src/main/java/br/com/digitalhouse/desafiospringapi/usecase/model/response/PostResponse.java
+++ src/main/java/br/com/digitalhouse/desafiospringapi/usecase/model/response/PostResponse.java
@@ -1,30 +1,38 @@
 package br.com.digitalhouse.desafiospringapi.usecase.model.response;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class PostResponse implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer postId;
-    private LocalDate date;
+    private String date;
     private ProductResponse detail;
     private Integer category;
     private Double price;
 
     public PostResponse(Integer postId, LocalDate date, ProductResponse detail, Integer category, Double price) {
         this.postId = postId;
-        this.date = date;
         this.detail = detail;
         this.category = category;
         this.price = price;
+        convertDate(date);
+    }
+
+    private void convertDate(LocalDate localDate) {
+        var pattern = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        var date = localDate.format(pattern);
+        this.date = date;
     }
 
     public Integer getPostId() {
         return postId;
     }
 
-    public LocalDate getDate() {
+    public String getDate() {
         return date;
     }
 
